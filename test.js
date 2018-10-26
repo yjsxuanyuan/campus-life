93ce27c71cc0e516007cd08ef5f27b1b9fc14810
yjsxuanyuan <yjsxuanyuan@126.com>
2018-10-26 19:57:52 +0900

    first commit

---
+++ test.js
@@ -0,0 +1,21 @@
+$(document).ready(()=>{
+  var arrow_index = 0;
+  var content_width = parseInt($(".public-service-icons").width());
+  $(window).resize(()=>{
+    content_width = parseInt($(".public-service-icons").width());
+      $(".icons-part").css('transform','translateX(' + -arrow_index*content_width + 'px)');
+  });
+  $(".arrow").click(function(){
+    if ($(this).hasClass("go-left") && arrow_index) { arrow_index--; }
+    if ($(this).hasClass("go-right") && arrow_index<1) { console.log(1);arrow_index++; }
+    $(".icons-part").css('transform','translateX(' + -arrow_index*content_width + 'px)');
+    $(".dot").css("opacity","0.5");
+    $("#dot"+arrow_index).css("opacity","1");
+  });
+  $(".dot").click(function(){
+    var arrow_index = parseInt(this.id.slice(3));
+    $(".icons-part").css('transform','translateX(' + -arrow_index*content_width + 'px)');
+    $(".dot").css("opacity","0.5");
+    $(this).css("opacity","1");
+  })
+});
