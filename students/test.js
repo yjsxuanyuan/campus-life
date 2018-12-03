1c8e90af6924999d5997fb1227199f95c134b1fc
yjsxuanyuan <yjsxuanyuan@126.com>
2018-12-03 17:46:28 +0900

    emm

--- students/test.js
+++ students/test.js
@@ -2,6 +2,7 @@ $(document).ready(()=>{
     let totalWidth = parseInt($(".students-news").css("width"));
     let imgWidth = parseInt($(".students-one-new img").css("width"));
     $(".news-info").css("width", totalWidth - imgWidth + 20 + "px");
+    setTimeout(()=>{ $(".news-info").css("width", totalWidth - imgWidth + 20 + "px");}, 200);
     $(window).resize(
         ()=>{let totalWidth = parseInt($(".students-news").css("width"));
     let imgWidth = parseInt($(".students-one-new img").css("width"));$(".news-info").css("width", totalWidth - imgWidth + 20 + "px");}
